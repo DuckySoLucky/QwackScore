c62ca3c294232a8e5b2e0a5d96c6b2aee526e785
DuckySoLucky <robertkovac160@gmail.com>
2024-11-27 19:46:39 +0100

    bump API key

--- app/API/config.ts
+++ app/API/config.ts
@@ -8,5 +8,5 @@ export const config = {
   localAPI: 'http://192.168.0.109:3000',
   useLocalAPI: false,
 
-  sportRadarAPIKey: 'esJwZP76dHEQqBgBJgHQqUlkSI5SAaQg7o1rzvy0',
+  sportRadarAPIKey: 'PgFQ1EWumDwKFZjzmvs0LVYCol775Q5nsVH6kNWS',
 };
